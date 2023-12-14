628ffd5ea0c14ec2100227ea5959d92261e8c428
vbox_god <sussugoma.e@gmail.com>
2023-12-14 16:06:32 +0800

    Quicksort/compile.sh: added options to check memory leaks and run with a debugger(gdb)

--- Quicksort/compile.sh
+++ Quicksort/compile.sh
@@ -1,5 +1,23 @@
-# check memory (leaks/valgrind)
-# run gdb
+options=$1
 
-cc -Wall -Werror -Wextra *.c
-./a.out
+if [ "$#" -eq 0 ]; then
+	cc -Wall -Wpedantic -Werror -Wextra *.c
+	./a.out
+fi
+
+case $options in
+	# check memory leaks
+	"m")
+		cc -g -Wall -Wpedantic -Werror -Wextra *.c
+		valgrind ./a.out
+		;;
+	"M")
+		cc -g -Wall -Wpedantic -Werror -Wextra *.c
+		valgrind --leak-check=full --show-leak-kinds=all ./a.out
+		;;
+	# run gdb
+	"d")
+		cc -g -Wall -Wpedantic -Werror -Wextra *.c
+		gdb ./a.out
+		;;
+esac
