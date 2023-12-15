12406f49e73ff8102914d845357f0306945c3b0d
Tan Jun Yu <tjun-yu@u81z08s04.42kl.edu.my>
2023-12-15 11:17:19 +0800

    Quicksort/compile.sh: added option to run memmory check on macOs and refactored the script

--- Quicksort/compile.sh
+++ Quicksort/compile.sh
@@ -1,23 +1,23 @@
 options=$1
 
+cc g -Wall -Wpedantic -Werror -Wextra *.c
+
 if [ "$#" -eq 0 ]; then
-	cc -Wall -Wpedantic -Werror -Wextra *.c
 	./a.out
 fi
 
 case $options in
 	# check memory leaks
 	"m")
-		cc -g -Wall -Wpedantic -Werror -Wextra *.c
 		valgrind ./a.out
 		;;
 	"M")
-		cc -g -Wall -Wpedantic -Werror -Wextra *.c
 		valgrind --leak-check=full --show-leak-kinds=all ./a.out
 		;;
+	"l")
+		leaks -atExit -- ./a.out
 	# run gdb
 	"d")
-		cc -g -Wall -Wpedantic -Werror -Wextra *.c
 		gdb ./a.out
 		;;
 	# remove executable
