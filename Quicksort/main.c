288f831253c49154fb5f05c13392993387406975
Tan Jun Yu <tjun-yu@u81z08s04.42kl.edu.my>
2023-12-15 09:49:38 +0800

    main.c: updated main to generate array of random numbers

--- Quicksort/main.c
+++ Quicksort/main.c
@@ -1,22 +1,32 @@
 #include "quick_sort.h"
 
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
+void printArr(int arr[], int length);
 
 int main(void)
 {
-	int arr[10] = {3, 2, 0, 1, 4, 8, 7, 6, 9, 5};
-	int n		= sizeof(arr) / sizeof(arr[0]);		
+	int length = 20;
+	int arr[length];
+
+	srand(time(NULL));
+	for (int i = 0; i < length; ++i)
+		arr[i] = rand() % 100;	// generating random numbers between 0 & 99
 
 	printf("Before: ");
-	for (int i = 0; i < n; ++i)
-		printf("%d ", arr[i]);
-	printf("\n\n");
+	printArr(arr, length);
 
-	// DEBUG("n", n - 1);
-	quick_sort(arr, 0, n - 1);
+	quick_sort(arr, 0, length - 1);
 	
 	printf("After: ");
-	for (int i = 0; i < 10; ++i)
+	printArr(arr, length);
+}
+
+void printArr(int arr[], int length)
+{
+	for (int i = 0; i < length; ++i)
 		printf("%d ", arr[i]);
-	printf("\n");
+	printf("\n\n");
 }
